2612b4436449c2fb9aa15ee1c9c5481098bc8bf9
Jan Ruusuvuori <jan.ruusuvuori@kodan.fi>
2018-05-24 16:38:05 +0300

    try a trivial AmazonS3Client.headBucket call

--- src/clj_aws_s3_localstack_demo/core.clj
+++ src/clj_aws_s3_localstack_demo/core.clj
@@ -1,7 +1,21 @@
 (ns clj-aws-s3-localstack-demo.core
-  (:gen-class))
+  (:gen-class)
+  (:import [com.amazonaws.client.builder AwsClientBuilder$EndpointConfiguration]
+           [com.amazonaws.services.s3 AmazonS3Client AmazonS3ClientBuilder]
+           [com.amazonaws.services.s3.model HeadBucketRequest]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Checks if the bucket can be accessed"
   [& args]
-  (println "Hello, World!"))
+  (let [region                 "eu-west-1"
+        endpoint               (str "https://s3.eu-west-1.amazonaws.com")
+        bucket                 "clj-aws-s3-localstack-demo-bucket"
+        ^AmazonS3Client client (-> (AmazonS3ClientBuilder/standard)
+                                   (.enablePathStyleAccess)
+                                   (.withEndpointConfiguration (AwsClientBuilder$EndpointConfiguration. endpoint region))
+                                   (.build))
+        bucket-region (->> bucket
+                           (HeadBucketRequest.)
+                           (.headBucket client)
+                           (.getBucketRegion))]
+    (println "Success! Bucket" bucket "exists and is accessible in region" bucket-region)))
