4bd4852174586023ba5399ab39029729013bf77a
Jan Ruusuvuori <jan.ruusuvuori@kodan.fi>
2018-05-24 16:01:19 +0300

    Initial commit: result of 'lein new app'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-aws-s3-localstack-demo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_aws_s3_localstack_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-aws-s3-localstack-demo.core-test
+  (:require [clojure.test :refer :all]
+            [clj-aws-s3-localstack-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
