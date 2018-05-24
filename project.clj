06f40ed85c93da1c5ce81cad09a9f0b61ba9db5e
Jan Ruusuvuori <jan.ruusuvuori@kodan.fi>
2018-05-24 16:05:49 +0300

    add dependency: aws-java-sdk-s3 1.11.313

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[com.amazonaws/aws-java-sdk-s3 "1.11.313"]
+                 [org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clj-aws-s3-localstack-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
